9e2df722e4a67793e227d7ded5c37eef31db1a11
DMills94 <davidj-mills@live.co.uk>
2026-06-07 11:17:57 +0100

    feat: upgrade to tailwind v4.3

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="h-[var(--header-height)] w-full flex items-center px-4">
+    <header className="h-(--header-height) w-full flex items-center px-4">
       <h1>Minna Meals</h1>
     </header>
   );
