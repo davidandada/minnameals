6c853eb890357b0a14099e1b697cec49578ae018
DMills94 <davidj-mills@live.co.uk>
2026-06-07 17:42:19 +0100

    fix: return json for unauth in list_items

--- api/index.py
+++ api/index.py
@@ -16,21 +16,16 @@ app = Flask(__name__)
 def is_cookie_valid():
     auth_password = os.getenv("APP_PASSWORD")
     app_password = request.cookies.get('app_password')
-
     success = auth_password == app_password
-
     return success
 
 #------------------------------
 # ROUTES
 #------------------------------
 
-# Using set_cookie( ) method to set the key-value pairs below.
 @app.route('/v1/auth')
 def auth():
-    
     success = is_cookie_valid()
-
     return jsonify({
         "success": success
     })
@@ -39,6 +34,6 @@ def auth():
 def get_list_items():
     authenticated = is_cookie_valid()
     if not authenticated:
-        return "error", 401
+        return jsonify({ "message": "You are not authenticated "}), 401
     rows = supabase.schema("mealplan").table("list_items").select("*").eq("is_archived", False).order("id").execute()
     return jsonify(rows.data if hasattr(rows, "data") else rows)
