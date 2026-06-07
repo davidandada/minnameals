6b1c6542ebfb0191a9a1e08a2b70b17618b67152
adawor <adaworswick@gmail.com>
2026-06-07 16:04:26 +0100

    auth_route

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, make_response, jsonify
 from supabase import create_client
 
 #------------------------------
@@ -13,11 +13,32 @@ supabase = create_client(
 
 app = Flask(__name__)
 
+def is_cookie_valid():
+    auth_password = os.getenv("APP_PASSWORD")
+    app_password = request.cookies.get('app_password')
+
+    success = auth_password == app_password
+
+    return success
+
 #------------------------------
 # ROUTES
 #------------------------------
 
-@app.route("/todos", methods=["GET"])
-def get_todos():
-    rows = supabase.schema("mealplan").table("list_items").select("*").order("id").execute()
+# Using set_cookie( ) method to set the key-value pairs below.
+@app.route('/v1/auth')
+def auth():
+    
+    success = is_cookie_valid()
+
+    return jsonify({
+        "success": success
+    })
+
+@app.route("/v1/list_items", methods=["GET"])
+def get_list_items():
+    authenticated = is_cookie_valid()
+    if not authenticated:
+        return "error", 401
+    rows = supabase.schema("mealplan").table("list_items").select("*").eq("is_archived", False).order("id").execute()
     return jsonify(rows.data if hasattr(rows, "data") else rows)
