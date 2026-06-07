579c4041fc298a247912b797b678939ea96f5b2f
adawor <adaworswick@gmail.com>
2026-06-07 23:03:47 +0100

    Backend - API added POST and PATCH

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, jsonify
 from supabase import create_client
+from datetime import datetime, timezone
 
 #------------------------------
 # SUPERBASE PUBLIC DEMO KEY
@@ -16,7 +17,9 @@ app = Flask(__name__)
 def is_cookie_valid():
     auth_password = os.getenv("APP_PASSWORD")
     app_password = request.cookies.get('app_password')
+
     success = auth_password == app_password
+
     return success
 
 #------------------------------
@@ -25,7 +28,9 @@ def is_cookie_valid():
 
 @app.route('/v1/auth')
 def auth():
+    
     success = is_cookie_valid()
+
     return jsonify({
         "success": success
     })
@@ -37,3 +42,61 @@ def get_list_items():
         return jsonify({ "message": "You are not authenticated "}), 401
     rows = supabase.schema("mealplan").table("list_items").select("*").eq("is_archived", False).order("id").execute()
     return jsonify(rows.data if hasattr(rows, "data") else rows)
+
+@app.route("/v1/list_items", methods=["POST"])
+def create_list_items():
+    data = request.get_json() 
+    # { item: 'apples' }
+
+    if not "item" in data:
+        return jsonify({"error": "Item is required"}), 400
+
+    result = (
+        supabase.schema("mealplan").table("list_items")
+        .insert({
+            "item": data["item"]
+        })
+        .select("*")
+        .eq("is_archived", False)
+        .execute()
+    )
+
+    return jsonify(result.data)
+
+@app.route("/v1/list_items", methods=["PATCH"])
+def create_list_items():
+    data = request.get_json()
+    # { id: 1, item: 'apples', is_checked: true, is_archived: true }
+
+    if not "id" in data:
+        return jsonify({"error": "ID is required"}), 400
+
+    update_fields = {}
+
+    if "item" in data:
+        update_fields["item"] = data["item"]
+
+    if "is_checked" in data:
+        update_fields["is_checked"] = data["is_checked"]
+
+    if "is_archived" in data:
+        update_fields["is_archived"] = data["is_archived"]
+
+        if data["is_archived"] is True:
+            update_fields["archived_at"] = datetime.now(timezone.utc)
+
+    update_fields["updated_at"] = datetime.now(timezone.utc)
+
+    if not update_fields:
+        return jsonify({"error": "No fields to update"}), 400
+
+    result = (
+        supabase.schema("mealplan")
+        .table("list_items")
+        .update(update_fields)
+        .eq("id", data["id"])
+        .select("*")
+        .execute()
+    )
+
+    return jsonify(result.data)
