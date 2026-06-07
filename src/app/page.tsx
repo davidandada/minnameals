5d38992be1eb39e56b612c986fc844a0a5ba1819
DMills94 <davidj-mills@live.co.uk>
2026-06-07 12:40:18 +0100

    fix: base url uses vercel url if available

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 import ShoppingList from "../components/minnameals/ShoppingList";
 
 export default async function Home() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
   const res = await fetch(`${baseUrl}/api/todos`);
   if (!res.ok) {
     throw new Error("Failed to fetch list");
