b0486a68e606e6f8673bf96e860628f7dd199608
DMills94 <davidj-mills@live.co.uk>
2026-06-22 21:04:57 +0100

    fix: tanstack query extension and fix css import

--- src/app/QueryProvider.tsx
+++ src/app/QueryProvider.tsx
@@ -1,6 +1,14 @@
 "use client";
 
+import { useEffect } from "react";
 import { environmentManager, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// TypeScript only:
+declare global {
+  interface Window {
+    __TANSTACK_QUERY_CLIENT__: QueryClient;
+  }
+}
 
 function makeQueryClient() {
   return new QueryClient({
@@ -25,6 +33,12 @@ function getQueryClient() {
 
 export default function QueryProvider({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.__TANSTACK_QUERY_CLIENT__ = queryClient;
+    }
+  }, [queryClient]);
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
