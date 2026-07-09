5fdb7f55667ff69b7ee4c1e8939312c56a488825
adawor <adaworswick@gmail.com>
2026-07-09 22:48:39 +0100

    added category and route to POST and PATCH category, amended list_items to item and amended item POST and PATCH for category

--- api/helpers/messages.py
+++ api/helpers/messages.py
@@ -3,3 +3,4 @@ ITEM_REQUIRED = {"message": "Item is required"}
 NO_FIELD = {"message": "No field to update"}
 ID_REQUIRED = {"message": "ID is required"}
 POSITION_REQUIRED = {"message": "Position is required"}
+NAME_REQUIRED = {"message": "Name is required"}
