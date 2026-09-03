7a3acb676e71f1ac08dab85c3d94e9614774550a
dmills94 <davidj-mills@live.co.uk>
2026-09-03 23:24:44 +0100

    fix(password): vertically center password form in viewport

--- src/app/password/page.tsx
+++ src/app/password/page.tsx
@@ -2,7 +2,7 @@ import PasswordForm from "@/components/PasswordForm";
 
 export default async function Password() {
   return (
-    <section className="min-h-[inherit] w-full flex items-center justify-center">
+    <section className="flex-1 w-full flex items-center justify-center">
       <PasswordForm />
     </section>
   );
